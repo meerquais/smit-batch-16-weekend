ff394687a8a6d38d3d07694749f060b6aa65cf20
Meer Quais <meerquais017@gmail.com>
2025-06-22 11:58:39 +0500

    JavaScript started

---
+++ JavaScript/Class 01/app.js
@@ -0,0 +1,49 @@
+// alert("Welcome to JavaScript Class 01.");
+
+
+// alert("JavaScript is connected.");
+
+console.log("JavaScript Connect.");
+
+
+// var userName = "Meer Quais";
+
+// console.log(userName);
+
+// console.log(userName); // undefined
+// var userName = "Meer Quais"
+// console.log(userName); // Meer Quais
+
+
+// console.log(a);
+// var a = 10;
+
+
+// var a = 10; 
+// console.log(a);
+// var a = 20;
+// console.log(a);
+
+// a = 30;
+// console.log(a);
+
+// valid names for variable 
+
+var camelCase = "This is Camel Case";
+var snake_case = "This is snake Case";
+var PascalCase = "This is Pascal Case";
+var $NoCase = "This is $ sign variable name";
+
+console.log(camelCase);
+console.log(snake_case);
+console.log(PascalCase);
+console.log($NoCase);
+
+
+// invalid names for variable 
+
+// var 123name = "Meer" ;
+// var !user = "Name";
+// var var = "meer"
+// console.log(let);
+
