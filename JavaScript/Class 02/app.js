831a0bf98674c1ee64d4c03e2894311e756066be
Meer Quais <meerquais017@gmail.com>
2025-06-28 12:24:13 +0500

    Data Types

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,99 @@
+// alert("JavaScript Connected");
+// document.write("<p>Hello World</p>");
+
+// console.log("JS Connected.");
+
+
+// Declaration 
+// var name;
+// initization
+// name = "Meer Quais";
+
+// var fullName = "Meer";
+
+// console.log(fullName);
+
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+// Camel Case 
+
+// var camelCase = "This is Camel Case.";
+
+// Snake Case 
+
+// var snake_case = "This is Snake Case.";
+
+// Pascal Case
+
+// var PascalCase = "This is Pascal Case.";
+
+
+// Data Types 
+
+// Number 
+
+// var age = 23;
+
+// console.log(typeof age , age)
+
+// String 
+
+// var name = "Ali";
+
+// console.log(typeof name, name)
+
+// Boolean
+
+
+// var isActive = true;
+// var isLoggedIn = false;
+
+// console.log(isActive, typeof isActive);
+// console.log(isLoggedIn, typeof isLoggedIn);
+
+
+// Null
+
+// var result = null;
+
+// console.log(typeof result, result);
+
+
+// Undefined 
+
+
+// var x = undefined;
+
+// console.log(x, typeof x);
+
+
+// Math Expressions 
+
+// Addition = " + "
+
+// var a = 4 + 9 ;
+// var b = 5 + 8;
+
+// var result = a + b + 29;
+
+// console.log(result);
+
+// Sub = "  -  "
+
+var a = 20;
+var b = 10;
+
+var result = b - a ;
+
+console.log(result);
+
+
+
+
+
+
+
+
+
