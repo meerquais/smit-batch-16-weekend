21c3ce622a39048e177874987ffe32b070711c57
Meer Quais <meerquais017@gmail.com>
2025-06-29 11:58:47 +0500

    Math Expressions and Concatination

---
+++ JavaScript/Class 03/app.js
@@ -0,0 +1,213 @@
+// console.log("JavaScript Connected.");
+
+// console.log(20 + 4 , "Add");
+// console.log(12 - 6 , "Minus/Subtract");
+// console.log(6 * 4, "Multiply");
+// console.log(20 / 4, "Division");
+// console.log(7 % 3 , "Modulus");
+
+// var a = 10;
+
+// a += 5;
+
+// console.log(a);
+
+// var b = 100;
+
+// b -= 20;
+
+// console.log(b)
+
+// var c = 20;
+
+// c *= 2;
+
+// console.log(c);
+
+
+// var d = 9;
+// d %= 4;
+
+// console.log(d);
+
+
+// var e = 8;
+
+// e /= 2;
+// console.log(e);
+
+// var a = 50;
+// var b = 60;
+
+// var result = a + b;
+
+// console.log(result);
+// console.log(a % b);
+
+
+// var a = 10;
+// a++
+
+// // var b = a + 10;
+
+// console.log(a);
+
+// var a = 5;
+// console.log(++a);
+
+// var b = 5;
+// console.log(b++);
+// console.log(b)
+
+
+// var a = 5;
+// console.log(++a);
+
+// var b = 5;
+// console.log(b++);
+// console.log(b);
+
+
+
+// var a = 5; // 6 // 7 / 6 / 5 / 6
+//         //  5  +  7  -  6 + 6 + 6 = 18
+// var b = a++ + ++a - --a + a-- + ++a;
+
+// console.log(b);
+
+
+// var x =  3;// / 4 / 5 / 4 / 3 /4 
+// 	//4 + 4 +4 + 4 + 4 + 4 = 24
+// var y = ++x     +     x++    +     --x    +    x--    +    ++x    +   x;
+
+// console.log(y)
+
+// var m = 10;// / 11 / 12 / 11 / 10 / 11
+// 	//10 + 11 + 12 - 10 + 11 = 34 
+// let result = m++     +     m++   +    m--    -    --m    +    ++m
+ 
+// console.log(result)
+
+// var z = 2; // / 3 / 4 / 4 / 3/2 / 1 / 2 / 2 /3 /3
+// //	      3 	+     3 	+  4       +      3      +     3      -     1      -   2           + 2       + 3   +  3 = 21
+// let output = ++z    +    z++     +     z     +     --z    +    z--     -     --z    -     ++z    +    z    +    ++z     +     z++
+
+// console.log(output)
+
+
+// var result = (5 + 3) * 2;
+
+
+
+
+// var result = 10 + 2 * 5;
+
+
+// var result = 20 * 5 / 2 ; 
+
+
+// var result = (4 + 2) * (6 - 3) + 5 ** 2 ;
+
+// 1 . 4 + 2 = 6 
+// 2 . 6 - 3 = 3
+// 3 . 6 * 3 = 18
+// 4 . 5 ** 2 = 25
+// 5 . 18 + 25 = 43
+
+
+// var result = ((2 + 3) * 2 ) ** 2;
+
+// 1 . 2 + 3 = 5
+// 2 . 5 * 2 = 10;
+// 3 . 10 ** 2 = 100;
+
+
+// console.log(result)
+
+
+// alert("Hello Class!")
+
+// var message = "Hello Weekend Class!";
+
+// alert(message);
+
+
+// var user1 = "Meer";
+// var user2 = "Minhaj";
+// var user3 = "Hafeez";
+
+// alert("Welcome");
+// alert(user1)
+
+
+// var result = "Hello" + " World";
+
+
+// alert("Welcome " + user3 + " to the Class!")
+
+// console.log(result)
+
+// var a = "4";
+// var b = 4;
+
+// var result = a - b ;
+
+// console.log(result)
+
+// var first = "Hello";
+// var second = "World";
+
+// var result = first + " " + second;
+
+// console.log(result)
+
+
+// var name = "Ali";
+// var age = 22;
+
+// var result = name + " is " + age + " years old";
+
+// console.log(result);
+
+// var message = "Welcome ";
+
+// message += "to ";
+// message += "JavaScript!";
+
+// console.log(message);
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+
+// console.log(customMess);
+
+
+// var userName = prompt("Please Enter Your Name!" , "Guest!");
+
+// console.log(userName);
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter your last name");
+
+// var fullName = firstName + " " + lastName;
+
+// alert("Your Full Name is: " + fullName);
+
+// alert("Line 1 \n" + "Line 2"); 
+
+// var name = prompt("Type your Name");
+
+// document.write("Welcome " + name + " to the Class!");
+
+
+// Chap 1 Alerts
+
+// Q#1 
+
+// alert("Welcome to my Website");
+
+// Q#2 
+
+// Chap 2 VARIABLES FOR STRINGS
