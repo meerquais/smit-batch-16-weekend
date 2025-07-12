ba2037c05d990e635fa5cc956dbfa021f6885146
Meer Quais <meerquais017@gmail.com>
2025-07-12 11:34:14 +0500

    Conditions

---
+++ JavaScript/Class 04/app.js
@@ -0,0 +1,131 @@
+console.log("JavaScript is Connected!")
+
+// var firstName = "Meer";
+// var lastName = "Quais";
+
+
+
+// var fullName = firstName + " " + lastName ;
+
+
+// console.log(fullName);
+
+
+// var city = "Karachi";
+// var msg = "Welcome to " + city + "!";
+
+
+// document.write(msg)
+// console.log(msg);
+
+
+// var num1 = 20;
+// var num2 = 30;
+
+// var result = num1 + num2 ;
+
+// console.log(result);
+
+
+// var age = 25;
+
+// var string = "25";
+
+// var result = age + string;
+
+// console.log(result);
+
+
+// var message = "Today is";
+// message += " a beautiful";
+// message += " day!";
+
+// console.log(message);
+
+// var score = 95;
+// var result = "Your score is: " + score;
+// console.log(result);
+
+
+// var num1 = 5;
+// var num2 = "6";
+
+// var result = num1 == num2;
+// var result = num1 === num2;
+
+// var result = num1 != num2;
+// var result = num1 !== num2;
+
+
+
+// console.log(result)
+
+
+// if(condition){
+//   piece of code 
+// }else{
+//   piece of code
+// }
+
+
+// var age = 17;
+
+// if(age >= 18){
+//   console.log("Tum vote de sakhte ho.");
+// }else{
+//   console.log("Tum vote nahi de sakhte ho.");
+// }
+
+
+// if(condtion){
+//   block of code
+// }else if(condition){
+//   block of code
+// }else{
+//   block of code
+// }
+
+
+// var marks = 75;
+
+// if(marks >= 90){
+//   console.log("Grade A+");
+// }else if(marks >= 80){
+//   console.log("Grade A");
+// }else if(marks >=70){
+//   console.log("Grade B");
+// }else{
+//   console.log("Failed!");
+// }
+
+// var time = 11;
+
+// if(time < 12){
+//   console.log("Good Morning!");
+// }else if(time < 18){
+//   console.log("Good Afternoon");
+// }else{
+//   console.log("Good Evening!");
+// }
+
+// var temp = 35;
+
+// if(temp < 10){
+//   console.log("bohot thand hai.");
+// }else if(temp < 20){
+//   console.log("thand hai");
+// }else if(temp < 30){
+//   console.log("Normal hai.");
+// }else{
+//   console.log("Garmi zyada hai.");
+// }
+
+// var role = "admin";
+
+// if(role === "admin"){
+//   console.log("welcome Admin!");
+// }else if(role === "user"){
+//   console.log("welcome user!");
+// }else{
+//   console.log("welcome guest!");
+// }
