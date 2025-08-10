ed86aad263135a7733d1c25986b15d75e2f0b780
Meer Quais <meerquais017@gmail.com>
2025-08-10 11:58:47 +0500

    Revision

---
+++ JavaScript/Class 12/app.js
@@ -0,0 +1,85 @@
+console.log("JavaScript Connected!");
+
+// var num = 5.8;
+
+// console.log(Math.round(num)); // round up Method.
+
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.1));
+
+// console.log(Math.ceil(4.1));
+// console.log(Math.ceil(4.8));
+
+
+// var randomNum = Math.floor(Math.random() * 100) + 1;
+
+// var randomNum = Math.random();
+
+// var fixed = Number(randomNum.toFixed(2))
+
+// console.log(fixed);
+
+// var price = 49.999;
+
+// console.log(price.toFixed(2));
+
+
+// var userAge = prompt("Type your age");
+
+// if(userAge >= 18){
+//   console.log("User Allowd hai.");
+// }else{
+//   console.log("Not Allowed.");
+// }
+
+
+// var marks = +prompt("Type your marks");
+
+// if(marks >= 90){
+//   console.log("A+");
+// }else if(marks >= 80){
+//   console.log("A");
+// }else if(marks >= 70){
+//   console.log("B");
+// }else{
+//   console.log("Failed");
+// }
+
+
+// for(var i = 1; i <= 100; i++){
+//   console.log(i);
+// }
+
+
+// var arr = [1, 2, 3,4,5,6,7,8,9];
+// var count = 0;
+
+
+// for(var i = 0; i < arr.length; i++){
+//   count++
+//   console.log(i);
+// }
+
+// console.log(count);
+
+
+// console.log(arr.length);
+
+// var fruit1 = "Apple";
+// var fruit2 = "Orange";
+// var fruit3 = "Mango";
+// var fruit4 = "Banana";
+// var fruit5 = "Peaches";
+
+// var fruit = ["Apple" , "Mango" , "Strawberry" , "Banana" , "Orange",[],[],[],[]];
+
+// fruit[1] = "Watermelon";
+
+// fruit[10] = "Mango"
+
+
+// console.log(fruit);
+
+// console.log(fruit[2]);
+
+
