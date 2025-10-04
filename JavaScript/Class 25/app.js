249e80fe615f04b302d04c93ad3f5dbd58bbaa67
Meer Quais <meerquais017@gmail.com>
2025-10-04 11:52:34 +0500

    Objects

---
+++ JavaScript/Class 25/app.js
@@ -0,0 +1,96 @@
+console.log("JavaScript Connected!");
+
+// var plan1Name = "basic";
+// var plan1Price = 3.99;
+// var plan1Space = 100;
+// var plan1Date = 1000;
+// var plan1Page = 10;
+
+// var plan2Name = "Prof";
+// var plan2Price = 5.99;
+// var plan2Space = 500;
+// var plan2Date = 5000;
+// var plan2Page = 50;
+
+// var plan3Name = "ultimate";
+// var plan3Price = 9.99;
+// var plan3Space = 2000;
+// var plan3Date = 20000;
+// var plan3Page = 500;
+
+
+// var plan1 = {
+//   name:"Basic",
+//   price: 3.99,
+//   space: 100,
+//   data: 1000,
+//   page: 10
+// };
+// var plan2 = {
+//   name:"Prof",
+//   price:  5.99,
+//   space: 500,
+//   data: 5000,
+//   page: 50
+// };
+
+// plan1.discountMonths = [6,7,11];
+
+// console.log(plan1);
+
+
+// // dot notation
+
+// console.log(plan1.name);
+// console.log(plan1.price);
+// console.log(plan1.data);
+
+
+// var plan3 = {
+//   name:"Ultimate",
+//   price:9.99,
+//   space:2000,
+//   data:20000,
+//   pages:500
+// }
+
+// console.log(plan3);
+
+
+// plan3.discountMonths = [6,7,11];
+
+
+// var month = plan3.discountMonths[2];
+
+// console.log(month);
+
+// console.log(plan3.name,plan3.price,plan3.space,plan3.data,plan3.pages);
+
+
+// var car = {
+//   name:"Honda",
+//   model:"Civic",
+//   year:"2025",
+//   color:"black"
+// }
+
+
+// var UserPromt = prompt("Type your name");
+
+
+// var std = {
+//   name:UserPromt,
+// }
+
+// console.log(std);
+
+
+var std= {
+  names:["Meer" , "Hafeez" , "Ali" , "Minhaj"],
+  obj:{
+    name:"Meer Quais"
+  },
+}
+
+
+console.log(std.obj);
