ba91aec6699fb290ff0ad7d31f0132d92258f490
Meer Quais <meerquais017@gmail.com>
2025-11-29 11:55:47 +0500

    Login - signup - logout flow

---
+++ Advance JavaScript/Login-signup/dashboard.js
@@ -0,0 +1,27 @@
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+
+
+if(!currentUser){
+  window.location.href = 'login.html'
+}
+
+
+let logout = document.getElementById("btn-logout");
+
+
+logout.addEventListener("click", ()=>{
+  
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  localStorage.removeItem("currentUser");
+  window.location.reload
+
+  if(!currentUser){
+    window.location.href = "login.html"
+    
+  }
+
+
+
+})
